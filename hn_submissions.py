4066f19928b31828b3806fc39ed60a67e7d76a03
Dean <138621406+deantub@users.noreply.github.com>
2024-07-05 12:38:16 +0300

    hn2

--- hn_submissions.py
+++ hn_submissions.py
@@ -20,7 +20,7 @@ for submission_id in submission_ids[:30]:
 
     # Зробити словник для кожного допису
     submission_dict = {
-        'title':response_dict['title'],
+        'title': response_dict['title'],
         'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
         'comments': response_dict['descendants'],
     }
